e4753e064cc9569d7f9c18ea684db6d7d4d7a7e2
mysticrenji <mysticrenji@gmail.com>
2020-12-25 23:35:39 +0800

    Updated files

--- src/app.py
+++ src/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
 from flask_sqlalchemy import SQLAlchemy
-import os
+import os, pymysql
 
 
 app = Flask(__name__)
