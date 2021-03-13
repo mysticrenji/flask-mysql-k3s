bdab6d775b2d40be7797e0a74535bb0e4f444dcb
Renjith Ravindranathan <mysticrenji@users.noreply.github.com>
2021-03-13 23:58:38 +0800

    Update app.py

--- src/app.py
+++ src/app.py
@@ -6,13 +6,18 @@ import os
 app = Flask(__name__)
 app.secret_key = "Trapezium!1"
 # SqlAlchemy Database Configuration With Mysql 
-USER = os.getenv('DB_USER')
-PASSWORD = os.environ.get('DB_PASS')
-DB = os.environ.get('DB')
-HOST = os.environ.get('HOST')
-connectionstring = "mysql://"+USER+":"+PASSWORD+"@"+HOST+"/"+DB
-app.config['SQLALCHEMY_DATABASE_URI'] = connectionstring
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+#USER = os.getenv('DB_USER')
+#PASSWORD = os.environ.get('DB_PASS')
+#DB = os.environ.get('DB')
+#HOST = os.environ.get('HOST')
+#connectionstring = "mysql://"+USER+":"+PASSWORD+"@"+HOST+"/"+DB
+#app.config['SQLALCHEMY_DATABASE_URI'] = connectionstring
+#app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["MYSQL_DATABASE_USER"] = "root"
+app.config["MYSQL_DATABASE_PASSWORD"] = os.environ.get('DB_PASS')
+app.config["MYSQL_DATABASE_DB"] = os.environ.get('DB')
+app.config["MYSQL_DATABASE_HOST"] = os.getenv("MYSQL_SERVICE_HOST")
+app.config["MYSQL_DATABASE_PORT"] = int(os.getenv("MYSQL_SERVICE_PORT"))
 
 db = SQLAlchemy(app)
 
